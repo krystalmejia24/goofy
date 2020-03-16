3b6f14b31eae48f348412a2869a6455576da162f
Flávio Ribeiro <email@flavioribeiro.com>
2020-03-15 21:04:56 -0400

    load scheduler on page

--- ui/src/containers/Home/Home.tsx
+++ ui/src/containers/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Props } from './types';
 
+import GoofyScheduler from '../../components/Scheduler';
+
 const Home = (props: Props): JSX.Element => {
     return (
         <div>
-            goofy!
+            <GoofyScheduler />
         </div>
     );
 }
